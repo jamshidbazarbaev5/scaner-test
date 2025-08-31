de11ce2488f51fc20e497f7a803280c56abc13f8
jamshidbazarbaev5 <your-github-email@example.com>
2025-08-31 12:27:07 +0500

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
+import type { PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer';
+import { visualizer } from 'rollup-plugin-visualizer'
+
 export default defineConfig({
-  plugins: [react(),
-    visualizer({ open: true }),],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true
+    }) as PluginOption,
+  ],
   css: {
     postcss: './postcss.config.js',
   },
